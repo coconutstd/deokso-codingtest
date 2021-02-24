c1257f887f6771ea1aa83a3b2934c4e941ef47eb
junui <wnsspdl@naver.com>
2021-02-24 11:03:29 +0900

    큰수만들기, 베스트앨범 이준의

---
+++ 2021-02-22~2021-02-23/큰수만들기/이준의.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        String number = br.readLine();
+
+        Deque<Character> dq = new LinkedList<>();
+        for(int i = 0; i < number.length(); ++i){
+            while(k > 0 && !dq.isEmpty() && dq.getLast() < number.charAt(i)){
+                dq.removeLast();
+                k--;
+            }
+            dq.addLast(number.charAt(i));
+        }
+        StringBuilder ans = new StringBuilder();
+        while(dq.size() > k){
+            ans.append( dq.removeFirst());
+        }
+        bw.write(ans.toString() + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
