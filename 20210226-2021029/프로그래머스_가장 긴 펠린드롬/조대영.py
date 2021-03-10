19f138745f1f5b71ff51002f2bbbd7124a4a588c
daeyoungcho <DubleDstudy@users.noreply.github.com>
2021-03-10 10:20:39 +0900

    Update 조대영.py

--- 20210226-2021029/프로그래머스_가장 긴 펠린드롬/조대영.py
+++ 20210226-2021029/프로그래머스_가장 긴 펠린드롬/조대영.py
@@ -1,1 +1,14 @@
 
+def palindrome(string):
+    return string ==string[::-1]
+    
+def solution(s):
+    answer = 0
+    size=len(s)
+    ans=-1
+    for i in range(0,size):
+        for j in range(i,size+1):
+            if palindrome(s[i:j]):
+                if ans<len(s[i:j]):
+                    ans=len(s[i:j])
+    return ans
