6c5fd1635ae69a98c0a3b92e5cce367d45f4b966
junui <wnsspdl@naver.com>
2021-03-10 09:26:45 +0900

    야근지수,후위표기식, 가장긴팰린드롬

---
+++ 20210226-2021029/프로그래머스_가장 긴 펠린드롬/이준의.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    static boolean check(String subString){
+        int iter = subString.length() / 2 - 1;
+        int left = 0, right = subString.length() - 1;
+        for(int i = 0; i < iter; ++i){
+            if(subString.charAt(left++) != subString.charAt(right--)) return false;
+        }
+        return true;
+    }
+
+    public int solution(String s)
+    {
+        int max = -1;
+        for(int i = 0; i < s.length(); ++i){
+            if(check(s.substring(0, i + 1))){
+                if(max < i + 1) max = i + 1;
+            }
+        }
+        return max;
+    }
+}
--- 20210226-2021029/프로그래머스_야근지수/이준의.java
+++ 20210226-2021029/프로그래머스_야근지수/이준의.java
@@ -1,22 +1,22 @@
-class Solution
-{
-    static boolean check(String subString){
-        int iter = subString.length() / 2 - 1;
-        int left = 0, right = subString.length() - 1;
-        for(int i = 0; i < iter; ++i){
-            if(subString.charAt(left++) != subString.charAt(right--)) return false;
-        }
-        return true;
-    }
+import java.util.*;
 
-    public int solution(String s)
-    {
-        int max = -1;
-        for(int i = 0; i < s.length(); ++i){
-            if(check(s.substring(0, i + 1))){
-                if(max < i + 1) max = i + 1;
-            }
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        Integer works2[] = Arrays.stream(works).boxed().toArray(Integer[]::new);
+        Arrays.sort(works2, Collections.reverseOrder());
+        int index = 0;
+        int size = works2.length;
+        for(int i = 0; i < n; ++i){
+            if(index == size) index = 0;
+            works2[index++] -= 1;
         }
-        return max;
+
+
+        for(int i = 0; i < works.length; ++i){
+            if(works2[i] <= 0) continue;
+            answer += works2[i] * works2[i];
+        }
+        return answer;
     }
 }
