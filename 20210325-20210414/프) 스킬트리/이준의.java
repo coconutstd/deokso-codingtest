695f2c40ceef0c9b6ff08af1e4bb174fb2247708
junui <wnsspdl@naver.com>
2021-04-14 14:16:39 +0900

    재업로드

--- 20210325-20210414/프) 스킬트리/이준의.java
+++ 20210325-20210414/프) 스킬트리/이준의.java
@@ -2,17 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        ArrayList<String> skillTrees = new ArrayList<String>(Arrays.asList(skill_trees));
-        //ArrayList<String> skillTrees = new ArrayList<String>();
-        Iterator<String> it = skillTrees.iterator();
+        int answer = skill_trees.length;
+        HashSet<String> skill_set = new HashSet<>();
+        boolean[] alpha = new boolean[26];
+        for(int i = 0; i < skill.length(); ++i){
+            alpha[skill.charAt(i) - 'A'] = true;
+            skill_set.add(skill.substring(0, i + 1));
+        }
 
-        while (it.hasNext()) {
-            if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
-                it.remove();
+        for(int i = 0; i < skill_trees.length; ++i){
+            String skill_tree = skill_trees[i];
+            String temp = "";
+            for(int j = 0; j < skill_tree.length(); ++j){
+                if(alpha[skill_tree.charAt(j) - 'A']) temp += skill_tree.charAt(j);
+            }
+            if(temp.length() != 0 && !skill_set.contains(temp)) {
+                answer--;
             }
         }
-        answer = skillTrees.size();
+
         return answer;
     }
 }
